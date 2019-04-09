34de5d41ab2094e2e37a5d282cd30ebe662353d6
Shreya Kapoor <shreyakapoor98@gmail.com>
2019-04-09 22:50:14 +0530

    todolist_frontend.js checkfunction

--- public_static/todolist_frontend.js
+++ public_static/todolist_frontend.js
@@ -13,26 +13,39 @@
     })
 }
 
+function setDone(el) {
+    // when we tick any item we have an event and second we need to
+    //find which element we tick
+    if (el.checked) {
+
+    } else {
+
+    }
+}
+
 
 $(function () { //window.onload in jquery si simply writing function in dollar function
     let newTaskBox = $('#newtask')
     let addTaskBtn = $('#addtask')
     let todolistDiv = $('#todolist')
+    let inputCheckBox = $('input.todo-done')
+    console.log(inputCheckBox)
 
 
     function refreshTodoList (todos) {
         todolistDiv.empty();
 
         for (todo of todos) {
+            console.log(todo)
             let newTodoItem = $(`
             <div class="todoitem row col-12">
-            <input type="checkbox" class="col">
+            <input data-todoid="${todo.id}" onchange="setDone(this)" type="checkbox" class="col todo-done"> <!--jab bhi koi event hota hai toh window.event sends the event that is happening now -->
             <div class="col"></div>
-            <div class="col-10">$(todo.task)</div>
+            <div class="col-10">${todo.task}</div> 
             </div>`
             );
             todolistDiv.prepend(newTodoItem) // jis order mai fetch hua uske ulte order mai add ho jayega
-        }
+        };
     }
 
     getAllTodos((todos) => refreshTodoList(todos))
